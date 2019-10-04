99867dca226fc3afde8e620c33ee0d3fd5ddf7a4
AkhileshNS <nsakhilesh02@gmail.com>
2019-10-04 18:13:38 +0530

    Seperated Components and removed cloudinary-react

--- src/App/App.js
+++ src/App/App.js
@@ -3,24 +3,30 @@ import { Global } from '@emotion/core';
 
 import { globalStyles } from 'global/styles';
 
+function Video(path) {
+  return (
+    <video style={{ width: '100%' }} controls>
+      <source
+        src={`https://res.cloudinary.com/futuristinvent/video/upload/vc_h265,w_1280,c_limit/Test%20Videos/${path}.mp4`}
+        type='video/mp4; codecs=hvc1'
+      />
+      <source
+        src={`https://res.cloudinary.com/futuristinvent/video/upload/vc_vp9,w_1280,c_limit/Test%20Videos/${path}.webm`}
+        type='video/webm; codecs=vp9'
+      />
+      <source
+        src={`https://res.cloudinary.com/futuristinvent/video/upload/vc_auto,w_1280,c_limit/Test%20Videos/${path}.mp4`}
+        type='video/mp4'
+      />
+    </video>
+  );
+}
+
 function App() {
   return (
     <div className='App'>
       <Global styles={globalStyles} />
-      <video style={{width: "100%"}} controls>
-        <source
-          src='https://res.cloudinary.com/futuristinvent/video/upload/vc_h265,w_1280,c_limit/Test%20Videos/city.mp4'
-          type='video/mp4; codecs=hvc1'
-        />
-        <source
-          src='https://res.cloudinary.com/futuristinvent/video/upload/vc_vp9,w_1280,c_limit/Test%20Videos/city.webm'
-          type='video/webm; codecs=vp9'
-        />
-        <source
-          src='https://res.cloudinary.com/futuristinvent/video/upload/vc_auto,w_1280,c_limit/Test%20Videos/city.mp4'
-          type='video/mp4'
-        />
-      </video>
+      {Video("surfing")}
     </div>
   );
 }

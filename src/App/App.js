04e3c016923912219c7b073d19964e6fd7524e85
AkhileshNS <nsakhilesh02@gmail.com>
2019-10-04 17:53:51 +0530

    Created basic video streaming setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {Global} from '@emotion/core';
+import { Global } from '@emotion/core';
 
-import {globalStyles} from 'global/styles';
+import { globalStyles } from 'global/styles';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Global styles={globalStyles} />
-      <p>Hello World</p>
+      <video style={{width: "100%"}} controls>
+        <source
+          src='https://res.cloudinary.com/futuristinvent/video/upload/vc_h265,w_1280,c_limit/Test%20Videos/city.mp4'
+          type='video/mp4; codecs=hvc1'
+        />
+        <source
+          src='https://res.cloudinary.com/futuristinvent/video/upload/vc_vp9,w_1280,c_limit/Test%20Videos/city.webm'
+          type='video/webm; codecs=vp9'
+        />
+        <source
+          src='https://res.cloudinary.com/futuristinvent/video/upload/vc_auto,w_1280,c_limit/Test%20Videos/city.mp4'
+          type='video/mp4'
+        />
+      </video>
     </div>
   );
 }
